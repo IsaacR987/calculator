ebfdf238d049eda074bf1172680ca2a388d52ff3
isaacr987 <isaacr987@gmail.com>
2023-07-18 23:43:07 -0700

    Adds UI and event listeners for display/buttons.

---
+++ script.js
@@ -0,0 +1,70 @@
+function reset() {
+    content.textContent = "0";
+}
+
+function add(a, b) {
+    return a += b;
+}
+
+function subtract(a, b) {
+    return a -= b;
+}
+
+function divide(a, b) {
+    return a /= b;
+}
+
+function multiply(a, b) {
+    return a *= b;
+}
+
+function operate(a, b, sign) {
+    if (sign === "+") return add(a, b);
+    if (sign === "-") return subtract(a, b);
+    if (sign === "/") return divide(a, b);
+    if (sign === "*") return multiply(a, b);
+}
+
+let sign = "";
+let numArray = [];
+
+const content = document.querySelector('.displayText');
+const buttons = document.querySelectorAll('.num');
+content.textContent = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', changeDisplay);
+});
+
+function changeDisplay() {
+    if (content.textContent === "0") {
+        content.textContent = ""; 
+        content.textContent += this.textContent;
+    } else if (content.textContent === numArray[numArray.length-1]) {
+        content.textContent = "";
+        content.textContent += this.textContent;
+    } else {
+        content.textContent += this.textContent;
+    }
+};
+
+const signs = document.querySelectorAll(".sign");
+signs.forEach((keys) => {
+    keys.addEventListener('click', () => {
+        sign = keys.textContent;
+        numArray.push(content.textContent);
+        console.log(numArray);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
